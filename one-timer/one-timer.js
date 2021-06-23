6f565acc0f218dcaa38bdfc3c01325c0fe93d44d
menciuscheng <295567172@qq.com>
2021-06-23 17:48:44 +0800

    ONE TIMER

---
+++ one-timer/one-timer.js
@@ -0,0 +1,27 @@
+
+(function () {
+  window.setInterval(function () {
+    let t = document.getElementById('startTimestamp').value
+    if (!!t) {
+      let now = new Date().getTime()
+
+      let secondDiff = Math.round((now - t) / 1000)
+      // let second = secondDiff % 60
+      let minute = Math.round(secondDiff / 60)
+      if (minute > 0) {
+        document.getElementById('duration').value = minute + "分"
+      } else {
+        document.getElementById('duration').value = ""
+      }
+    }
+  }, 500)
+})()
+
+function start() {
+  let t = new Date()
+
+  document.getElementById('startTimestamp').value = t.getTime()
+
+  let s = t.toTimeString().substring(0, 5)
+  document.getElementById('startTime').value = s
+}
